fef73a1b0baa231f5138bb539c1cd3dfa5bde090
Nesma91 <nesmataha91@gmail.com>
2024-04-22 04:24:47 +0200

    Error Handling Phase of Skinet E-Commerce API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,11 +6,11 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
+using API.Errors;
 
 namespace API.Controllers;
-[Route("api/[controller]")]
-[ApiController]
-public class ProductsController : ControllerBase
+
+public class ProductsController : BaseApiController
 {
     private readonly IGenericRepository<Product> _productRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -41,11 +41,15 @@ public class ProductsController : ControllerBase
 }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
         var spec = new ProductsWithBrandsAndTypesSpecification(id);
 
         var product = await _productRepo.GetEntityWithSpecification(spec);
+
+        if (product == null) return NotFound(new ApiResponse(404));
 
         return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
     }
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -3,6 +3,10 @@
 namespace Core.Specifications;
 public class BaseSpecification<T> : ISpecification<T>
 {
+    public Expression<Func<T, bool>> Criteria { get; }
+
+    public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
     public BaseSpecification()
     {
         
@@ -11,10 +15,6 @@ public class BaseSpecification<T> : ISpecification<T>
     {
         Criteria = criteria;
     }
-
-    public Expression<Func<T, bool>> Criteria { get; }
-
-    public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
     protected void AddInclude(Expression<Func<T, object>> includeExpression)
     {
